1c695445ecb50adb03c723a0d5f3548414aae4cd
wyltw <138242311+wyltw@users.noreply.github.com>
2024-07-13 16:03:05 +0800

    finish and refactor proposition section

--- src/components/proposition-card.tsx
+++ src/components/proposition-card.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { ReactNode } from "react";
 
-export default function PropositionCard() {
-  return <div>PropositionCard</div>;
+type PropositionCardProps = {
+  imageAlt: string;
+  imageSrc: string;
+  label: string;
+  children: ReactNode;
+  textLines: string[];
+  className?: string;
+};
+
+export default function PropositionCard({
+  imageAlt,
+  imageSrc,
+  label,
+  children,
+  textLines,
+  className,
+}: PropositionCardProps) {
+  return (
+    <section className="grid w-full max-w-7xl grid-cols-2 place-items-center rounded-2xl bg-accent px-4 py-14">
+      <Image src={imageSrc} alt={imageAlt} width={280} height={280} />
+      <div className={cn("max-w-md", className)}>
+        <PropositionCardlabel>{label}</PropositionCardlabel>
+        {children}
+        <PropositionCardTextLines>
+          {textLines.map((textLine) => (
+            <p key={textLine}>{textLine}</p>
+          ))}
+        </PropositionCardTextLines>
+      </div>
+    </section>
+  );
+}
+
+function PropositionCardlabel({ children }: { children: React.ReactNode }) {
+  return <p className="mb-2 tracking-wider text-secondary">{children}</p>;
+}
+
+function PropositionCardTextLines({ children }: { children: React.ReactNode }) {
+  return <div className="whitespace-nowrap text-black/50">{children}</div>;
 }
--- src/components/selling-proposition.tsx
+++ src/components/selling-proposition.tsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import PropositionCard from "./proposition-card";
 
 export default function SellingProposition() {
   return (
-    <section>
-      <h2 className="text-primary mt-28 text-center text-4xl">
-        探索音樂，分享快樂
-      </h2>
-      <PropositionCard />
-    </section>
+    <>
+      <section className="flex flex-col items-center justify-center gap-y-8 px-36 py-28">
+        <h2 className="text-center text-4xl font-semibold text-primary">
+          探索音樂，分享快樂
+        </h2>
+
+        <PropositionCard
+          imageSrc="/images/nft-marketplace.svg"
+          imageAlt="treasure"
+          label="UNEARTHING"
+          textLines={[
+            "與他人分享自己喜愛的音樂",
+            "在這裡不用煩惱自己的喜好不被接受。",
+          ]}
+        >
+          <PropositionCardHeading>
+            發現
+            <span className="text-secondary">埋藏的</span>
+            音樂寶藏
+          </PropositionCardHeading>
+        </PropositionCard>
+        <PropositionCard
+          imageSrc="/images/discussion.svg"
+          imageAlt="discussion"
+          label="CONNECTING"
+          textLines={[
+            "身邊沒有喜歡後硬核，UK hardcore的朋友？",
+            "或者自己的音樂喜好還停留在上個世紀？",
+            "在這裡，你可以找到任何音樂風格的愛好者。",
+          ]}
+          className="order-first"
+        >
+          <PropositionCardHeading>
+            聚集
+            <span className="text-secondary">志同道合的</span>
+            愛好者
+          </PropositionCardHeading>
+        </PropositionCard>
+      </section>
+    </>
   );
+}
+
+function PropositionCardHeading({ children }: { children: ReactNode }) {
+  return <h3 className="whitespace-nowrap text-3xl font-medium">{children}</h3>;
 }
