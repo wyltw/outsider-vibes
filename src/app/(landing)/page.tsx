65dbe3bbfd7e8d7d65e4f38cc070c3d0f22e8986
wyltw <138242311+wyltw@users.noreply.github.com>
2024-07-18 16:05:47 +0800

    install framer-motion

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,9 +2,7 @@ import BackgroundPattern from "@/components/background-pattern";
 import CallToAction from "@/components/call-to-action";
 import Feature from "@/components/feature";
 import FinalCallToAction from "@/components/final-call-to-action";
-
 import SellingProposition from "@/components/selling-proposition";
-import Image from "next/image";
 
 export default function LandingPage() {
   return (
