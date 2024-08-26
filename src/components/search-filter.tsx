3d9f610aafb01622e44e48faef93723c297dbe3e
wyltw <138242311+wyltw@users.noreply.github.com>
2024-08-26 12:17:00 +0800

    fix: move top-[80px] to sidebar component instead of main in layout.

--- src/components/search-filter.tsx
+++ src/components/search-filter.tsx
@@ -3,7 +3,7 @@ import {
   CollapsibleTrigger,
   CollapsibleContent,
 } from "@/components/ui/collapsible";
-import { SlidersHorizontal } from "lucide-react";
+import { ChevronLeft, ChevronRight, SlidersHorizontal } from "lucide-react";
 import React, { ReactNode } from "react";
 import { Button } from "./ui/button";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
@@ -23,19 +23,17 @@ export default function SearchFilter({
             <span className="text-base text-primary">篩選條件</span>
           </Button>
         </CollapsibleTrigger>
-        <CollapsibleContent>
-          <div className="mt-2 space-y-3 rounded-md bg-white py-4 shadow">
-            <Filter text="流派：">
-              {genreList.map((genre) => (
-                <PillButton>{genre}</PillButton>
-              ))}
-            </Filter>
-            <Filter text="風格：">
-              {styleList.map((style) => (
-                <PillButton>{style}</PillButton>
-              ))}
-            </Filter>
-          </div>
+        <CollapsibleContent className="mt-2 max-w-max space-y-3 rounded-md bg-white py-4 shadow">
+          <Filter text="流派：">
+            {genreList.map((genre) => (
+              <PillButton>{genre}</PillButton>
+            ))}
+          </Filter>
+          <Filter text="風格：">
+            {styleList.map((style) => (
+              <PillButton>{style}</PillButton>
+            ))}
+          </Filter>
         </CollapsibleContent>
       </Collapsible>
     </>
@@ -46,13 +44,21 @@ type FilterProps = { children: ReactNode; text: string };
 
 function Filter({ children, text }: FilterProps) {
   return (
-    <div className="ms-3">
-      <p className="mb-2 text-nowrap text-primary">{text} </p>
-      <ScrollArea>
-        <div className="flex justify-start gap-x-2 py-2">
-          {children} <ScrollBar orientation="horizontal" />
-        </div>
-      </ScrollArea>
+    <div className="ms-3 text-primary">
+      <p className="mb-2 text-nowrap">{text} </p>
+      <div className="flex">
+        <Button size={"icon"} variant={"ghost"}>
+          <ChevronLeft className="" />
+        </Button>
+        <ScrollArea className="feather-edge rounded-3xl">
+          <div className="flex justify-start gap-x-2 py-2">
+            {children} <ScrollBar orientation="horizontal" />
+          </div>
+        </ScrollArea>
+        <Button size={"icon"} variant={"ghost"}>
+          <ChevronRight className="" />
+        </Button>
+      </div>
     </div>
   );
 }
