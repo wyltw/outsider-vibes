5783392b43f228ded75509485ff84a055a6d203e
wyltw <138242311+wyltw@users.noreply.github.com>
2024-10-05 16:55:11 +0800

    layout: add a illustration for authentication

--- src/app/(app)/user-collection/page.tsx
+++ src/app/(app)/user-collection/page.tsx
@@ -1,11 +1,23 @@
 import { auth } from "@/auth";
 import CustomError from "@/components/custom-error";
+import Image from "next/image";
 import React from "react";
 
 export default async function UserCollectionPage() {
   const session = await auth();
   if (!session?.user) {
-    return <CustomError error="請先登入後再查看收藏" />;
+    return (
+      <>
+        <CustomError error="請先登入後再查看收藏" />
+        <Image
+          className="me-auto ms-auto"
+          width={560}
+          height={560}
+          alt="authentication"
+          src={"/images/authentication-pana.svg"}
+        />
+      </>
+    );
   }
   return (
     <>
